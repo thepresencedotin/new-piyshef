ff15106d5b4526da0cd0cc43e3dea1d138616aa6
bhaveshgoswami02 <bhaveshgoswami2381998@gmail.com>
2021-03-15 10:38:35 +0530

    changs

--- user/js/lottie-images.js
+++ user/js/lottie-images.js
@@ -13,3 +13,12 @@ var animation = bodymovin.loadAnimation({
     autoplay: true,
     path: 'assets/json/cloud2.json'
 })
+
+var animate = bodymovin.loadAnimation({
+    container: document.getElementById('logo__videos'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/json/logo.json'
+})
+// animate.setSpeed(0.5);
